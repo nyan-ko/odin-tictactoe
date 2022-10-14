0221111c6e96a2a74c2a391dd0800f9c3d0ea459
nyan-ko <dumbstupidcatboy@gmail.com>
2022-10-14 12:41:31 -0400

    Add tictactoe board and basic functionality

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const Game = (function() {
+const Board = (function() {
     let board = [];
     let winner = "";
+    let players = ['X', 'O']
+    let currentPlayer = true;
+
     const reset = function() {
         for (let i = 0; i < 3; i++) {
             board[i] = ['', '', ''];
         }
     }
 
-    const markTile = function(x, y, type) {
+    const getGrid = function() {
+        if (board) {
+            return board;
+        }
+        reset();
+        return board;
+    }
+
+    const getCurrentPlayerType = function() {
+        return players[+currentPlayer];
+    }
+
+    const playerMarkTile = function(x, y) {
+        won = _markTile(+x, +y, getCurrentPlayerType());
+        if (!won) {
+            _alternatePlayer()
+        }
+        else {
+            alert(won + " has won.")
+        }
+    }
+
+    const _markTile = function(x, y, type) {
         if (winner) {
             return winner;
         }
@@ -19,27 +44,21 @@ const Game = (function() {
             return "invalid";
         }
 
-        let won = _checkWin(x, y);
-        if (won) {
+        if (_checkWin(x, y)) {
             winner = type;
             return winner;
         }
-        return "";
     }
 
-    const getGrid = function() {
-        if (board) {
-            return board;
-        }
-        reset();
-        return board;
+    const _alternatePlayer = function() {
+        currentPlayer = !currentPlayer;
     }
 
     const _checkWin = function(x, y) {
         let val = board[x][y];
         let win = false;
-        if (x === y || x === 0 && y === 2 || x === 2 && y === 0) {
-            win = false || _diagonalCheck(val);
+        if (x === y || (x === 0 && y === 2) || (x === 2 && y === 0)) {
+            win = _diagonalCheck(val);
         }
 
         win = win || _columnCheck(x, val) || _rowCheck(y, val);
@@ -70,6 +89,32 @@ const Game = (function() {
     }
 
     return {
-        reset, markTile, getGrid
+        reset, getGrid, playerMarkTile, getCurrentPlayerType
     }
 })();
+
+let grid = document.querySelector(".ttt");
+Board.reset();
+
+for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.setAttribute("data-x", x);
+        square.setAttribute("data-y", y);
+        square.addEventListener('click', onClick)
+        grid.appendChild(square);
+    }
+}
+
+function onClick(e) {
+    let x = this.getAttribute('data-x');
+    let y = this.getAttribute('data-y');
+    
+    Board.playerMarkTile(x, y)
+    updateVisualAfterTurn(this, x, y)
+}
+
+function updateVisualAfterTurn(square, x, y) {
+    square.innerText = Board.getGrid()[x][y]
+}
